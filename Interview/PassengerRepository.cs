04579e6825c99b7ac1f7a8b06abca0191f3196b8
Arvinder <arvinder_kamboo@hotmail.com>
2020-02-16 11:26:58 +0000

    Update Delete() to handle null parameter passed as argument
    Functionality added to get Repisitory_Save_NullItemPassedToSave() to pass

--- Interview/PassengerRepository.cs
+++ Interview/PassengerRepository.cs
@@ -33,6 +33,9 @@ namespace Interview
 
         public void Save(Passenger item)
         {
+            if (item == null)
+                throw new ArgumentNullException("item");
+
             if ((from g in _passengerList where g.Id == item.Id select g).Count() != 0)
                 throw new InvalidOperationException("Item Id already exists");
 
